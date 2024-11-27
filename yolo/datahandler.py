3b1bf389332a243580370d8ce62c74242720e792
Lisaag <lisatgroen@hotmail.com>
2024-11-27 10:32:13 +0100

    rectangle debug6

--- yolo/datahandler.py
+++ yolo/datahandler.py
@@ -104,8 +104,9 @@ def create_yolo_labels(is_dummy:bool = False):
 
 
 def test_bb(file_name, x, y, w, h):
-    im_path = os.path.join(os.path.abspath(os.getcwd()), settings.slapi_dir, "raw", "images", file_name)
-    #print(im_path)
+    im_path = os.path.join(os.path.abspath(os.getcwd()), settings.slapi_dir, "raw", "vis", file_name)
+    image = cv2.imread(im_path, cv2.IMREAD_COLOR)
+    if(image is None): im_path = os.path.join(os.path.abspath(os.getcwd()), settings.slapi_dir, "raw", "images", file_name)
     image = cv2.imread(im_path, cv2.IMREAD_COLOR)
 
     #To draw a rectangle, you need top-left corner and bottom-right corner of rectangle.
