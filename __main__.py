9e61be430b40620c3960e90e31be1503ab9fde32
Lisaag <lisatgroen@hotmail.com>
2024-12-09 15:55:09 +0100

    added detect args

--- __main__.py
+++ __main__.py
@@ -1,10 +1,12 @@
 import argparse
 from yolo import YOLOlabeler
 import datasplitter
+import detect
 
 PARSER = argparse.ArgumentParser(prog='EyDetector')
 PARSER.add_argument('--YOLOlabel', nargs=2, type=str, help="convert annotation csv to yolo labels. arg1['real', 'dummy'], arg2['aabb', 'obb', 'ocaabb']")
-PARSER.add_argument('--datasplit', nargs=1, type=str, help="split the data into train, validation, and test sets")
+PARSER.add_argument('--datasplit', nargs=1, type=str, help="split the data into train, validation, and test sets. arg1['aabb', 'obb', 'ocaabb']")
+PARSER.add_argument('--detect', nargs=1, type=str, help="detect on videos, using trained model. arg1[RELATIVE PATH TO WEIGHTS]")
 
 if __name__ == '__main__':
     args = PARSER.parse_args() # type: ignore
@@ -18,6 +20,9 @@ if __name__ == '__main__':
     elif(args.datasplit):
         annotation_type = args.datasplit[0]
         datasplitter.split_dataset(annotation_type)
+
+    elif(args.detect):
+        datasplitter.split_dataset(args.detect)
 
        
 
