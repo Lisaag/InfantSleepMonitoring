fe8193abce8c436c88f6759b3dbd91a9ba15bb8f
Lisaag <lisatgroen@hotmail.com>
2024-12-10 23:51:40 +0100

    bugfix

--- dataaugmenter.py
+++ dataaugmenter.py
@@ -9,13 +9,11 @@ destination_dir = os.path.join(os.path.abspath(os.getcwd()), "datasets","SLAPI",
 def augment_brightness():
     print("augment_brightness() called")
     for img in glob.glob(images_dir + "/*.jpg"):
+        file_name = os.path.basename(img)
         image = cv2.imread(img)
         bright = np.ones(image.shape, dtype="uint8") * 70
         brightincrease = cv2.add(image,bright)
-        cv2.imwrite(os.path.join(destination_dir, "augmentations", "brightness", img), brightincrease)
-        print(str(img))
-        print(os.path.join(destination_dir, "augmentations", "brightness"))
-
+        cv2.imwrite(os.path.join(destination_dir, "augmentations", "brightness", file_name), brightincrease)
 
 def augment_all_images():
     augment_brightness()
