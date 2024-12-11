a280c25b27a2ac5eb5a0461f7dcf0c08981c77b6
Lisaag <lisatgroen@hotmail.com>
2024-12-11 16:28:12 +0100

    test obb detect

--- detect.py
+++ detect.py
@@ -113,7 +113,7 @@ def detect_vid_obb(relative_weights_path:str):
                     print(obb.xyxyxyxy)
                     print("B")
                     #obb.cpu()
-                    print(obb.xyxyxyxy.argmax())
+                    print(obb.xyxyxyxy.cpu().data.numpy())
 
                     # pts = np.array([[all_points_x[0], all_points_y[0]],[all_points_x[1], all_points_y[1]],[all_points_x[2], all_points_y[2]],[all_points_x[3], all_points_y[3]]], np.int32)
                     # pts = pts.reshape((-1,1,2))
