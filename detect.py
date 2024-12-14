ece3098fca1f7d67e346817fc29e099e249fdb53
Lisaag <lisatgroen@hotmail.com>
2024-12-14 11:48:11 +0100

    tracking test

--- detect.py
+++ detect.py
@@ -7,21 +7,12 @@ from itertools import chain
 from collections import defaultdict
 
 
-# Load the YOLO model
-#weights_path = os.path.join(os.path.abspath(os.getcwd()), "runs", "detect", "train2", "weights", "best.pt")
-
-# # Define the paths
-
-# #video_input_path = os.path.join(os.path.abspath(os.getcwd()), "vid","1.mp4")
-# output_video_path = os.path.join(os.path.abspath(os.getcwd()), "vid","output.mp4")
-
 def track_vid_aabb(relative_weights_path:str):
     weights_path = os.path.join(os.path.abspath(os.getcwd()), relative_weights_path)
     model = YOLO(weights_path)
     IN_directory = os.path.join(os.path.abspath(os.getcwd()), "vid", "IN")
-    OUT_directory = os.path.join(os.path.abspath(os.getcwd()), "vid", "OUT")
+
     for filename in os.listdir(IN_directory):
-        print(filename)
         video_output_path =  os.path.join(os.path.abspath(os.getcwd()), "vid", "OUT", "OUT"+str(filename))
         video_input_path =  os.path.join(os.path.abspath(os.getcwd()), "vid", "IN", filename)
         # Open the video file
@@ -29,23 +20,20 @@ def track_vid_aabb(relative_weights_path:str):
 
         # Store the track history
         track_history = defaultdict(lambda: [])
+        current_track_id = -1
+        max_track_epoch = 15
+        current_track_epoch = 0
 
         # Get video properties
         frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
         frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        fps = int(cap.get(cv2.CAP_PROP_FPS))
-
-        # Define the codec and create VideoWriter object
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        out = cv2.VideoWriter(video_output_path, fourcc, fps, (frame_width, frame_height))
 
         # Process each frame
         while cap.isOpened():
             ret, frame = cap.read()
             if not ret:
                 break
-
-            # Make predictions
+            
             results = model.track(frame, verbose=False, persist=True)
 
             # Draw predictions on the frame
@@ -53,46 +41,27 @@ def track_vid_aabb(relative_weights_path:str):
 
                 boxes = result.boxes  # Get bounding boxes
 
+                if (current_track_epoch == max_track_epoch):
+                    for key in track_history:
+                        print(f'Track idea {key} is {len(track_history[key])} times detected within 15 frames')
+                    track_history = defaultdict(lambda: [])
+
                 if(boxes.id == None): continue
                 
                 track_ids = boxes.id.int().cpu().tolist()
 
-
                 for box, track_id in zip(boxes, track_ids):
-                    x1, y1, x2, y2 = map(int, box.xyxy[0])  # Bounding box coordinates
-                    x = int((x1 + x2) / 2)
-                    y = int((y1 + y2) / 2)
-                    # track = track_history[track_id]
-
-
-
-                    # track.append((float(x), float(y)))  # x, y center point
-                    # if len(track) > 30:  # retain 90 tracks for 90 frames
-                    #     track.pop(0)
-
                     conf = box.conf[0]  # Confidence score
-                    cls = box.cls[0]  # Class index
 
-                    boxColor = (255, 0, 0)
-                    if cls == 0: boxColor = (0, 255, 0)
-                    # Draw bounding box
-                    cv2.rectangle(frame, (x1, y1), (x2, y2), boxColor, 2)
-                    label = f"{model.names[int(cls)]} {conf:.2f} {track_id:.2f}"  # Class label and confidence
-                    cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.0, boxColor, 2)
+                    track_history[track_id].append(conf)
 
-            # Write the frame to the output video
-            out.write(frame)
 
-            # Optional: Display the frame (comment out if not needed)
-            # cv2.imshow('YOLO Prediction', frame)
-            # if cv2.waitKey(1) & 0xFF == ord('q'):
-            #     break
+                current_track_epoch += 1             
 
         # Release resources
         cap.release()
-        out.release()
         cv2.destroyAllWindows()
-        print(f"Processed video saved at {video_output_path}")
+        print(f"Finish processing {video_output_path}")
 
 
 
