9ce7b70c2f23b0c8910eaa288cc3634caa36e576
Lisaag <lisatgroen@hotmail.com>
2025-01-02 17:41:57 +0100

    refactor

--- dataaugmenter.py
+++ dataaugmenter.py
@@ -40,12 +40,44 @@ def test_aabb(file_name, x_n, y_n, w_n, h_n):
     w = w_n * width
     h = h_n * height
 
-
     #To draw a rectangle, you need top-left corner and bottom-right corner of rectangle.
     cv2.rectangle(image, (int(x-(w/2)), int(y-(h/2))), (int(x+(w/2)), int(y+(h/2))), (0,255,0), 3)
     cv2.circle(image,(int(x-(w/2)), int(y-(h/2))), 10, (255,0,0), -1)
     if not cv2.imwrite( os.path.join(aug_vis_dir, file_name + ".jpg"), image):
         print("imwrite failed")
+
+def write_augmented(file_name, image, bboxes):
+    cv2.imwrite(os.path.join(aug_images_dir, file_name + ".jpg"), image)
+
+    with open(os.path.join(aug_labels_dir, file_name + ".txt"), "a") as file:
+        pass
+
+    for bbox in bboxes:
+        with open(os.path.join(aug_labels_dir, file_name + ".txt"), "a") as file:
+            file.write(str(int(bbox[4])) + " " + str(bbox[0]) + " " + str(bbox[1]) + " " + str(bbox[2]) + " " + str(bbox[3]) + " " + "\n")
+
+def albumentation_label(file_name):
+    with open(os.path.join(labels_dir, file_name+".txt"), 'r') as file:  # Open the file in read mode
+        content = file.read()  # Read the entire content of the file
+        label_data = [list(map(float, line.split())) for line in content.strip().split("\n")]
+        aug_labels = [row[1:] + [row[0]] for row in label_data]
+    
+    return aug_labels
+
+def augment_crop(file_name, transform_crop, prefix):   
+    image = cv2.imread(file_name + ".jpg")
+    aug_labels = albumentation_label(file_name)
+
+    for i in range(30):
+        transformed = transform_crop(image=image, bboxes=aug_labels)
+        transformed_image = transformed['image']
+        transformed_bboxes = transformed['bboxes']
+
+        if len(transformed_bboxes) > 0:
+            break
+
+    aug_filename = prefix + file_name
+    write_augmented(aug_filename, transformed_image, transformed_bboxes)
 
 
 def augment_albumentation():
@@ -53,37 +85,14 @@ def augment_albumentation():
     delete_files_in_directory(aug_images_dir)
     delete_files_in_directory(aug_vis_dir)
 
-    transform = A.Compose([
+    transform_crop = A.Compose([
         A.RandomCrop(width=750, height=550),
-        A.RandomBrightnessContrast(p=0.2),
     ], bbox_params=A.BboxParams(format='yolo', min_visibility=0.8))
 
     for img in glob.glob(images_dir + "/*.jpg"):
         file_name = re.sub(r'\.jpg$', '', os.path.basename(img))
-        with open(os.path.join(labels_dir, file_name+".txt"), 'r') as file:  # Open the file in read mode
-            content = file.read()  # Read the entire content of the file
-            label_data = [list(map(float, line.split())) for line in content.strip().split("\n")]
-            aug_labels = [row[1:] + [row[0]] for row in label_data]
-        
-        image = cv2.imread(img)
+        augment_crop(file_name, transform_crop, "CROP_")
 
-        for i in range(30):
-            transformed = transform(image=image, bboxes=aug_labels)
-            transformed_image = transformed['image']
-            transformed_bboxes = transformed['bboxes']
-
-            if len(transformed_bboxes) > 0:
-                break
-
-        aug_filename = "AUG" + file_name
-        cv2.imwrite(os.path.join(aug_images_dir, aug_filename + ".jpg"), transformed_image)
-
-        with open(os.path.join(aug_labels_dir, aug_filename + ".txt"), "a") as file:
-            pass
-
-        for bbox in transformed_bboxes:
-            with open(os.path.join(aug_labels_dir, aug_filename + ".txt"), "a") as file:
-                file.write(str(int(bbox[4])) + " " + str(bbox[0]) + " " + str(bbox[1]) + " " + str(bbox[2]) + " " + str(bbox[3]) + " " + "\n")
             # test_aabb(file_name, bbox[0], bbox[1], bbox[2], bbox[3])
         #TODO write transformed labels to file
         #TODO draw images with transformed bbox, to check validity of transformed bbox (make new directory to save these (vis))
