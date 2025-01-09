b2c026b5a006252a3abf6f9afb4f1d0dd4bb3603
Lisaag <lisatgroen@hotmail.com>
2025-01-09 11:12:39 +0100

    script to cut video fragments for REM dataset

--- __main__.py
+++ __main__.py
@@ -4,6 +4,7 @@ import datasplitter
 import detect
 import dataaugmenter
 import imgtransformer
+import videocutter
 
 PARSER = argparse.ArgumentParser(prog='EyDetector')
 PARSER.add_argument('--YOLOlabel', nargs=2, type=str, help="convert annotation csv to yolo labels. arg1['real', 'dummy'], arg2['aabb', 'obb', 'ocaabb', 'ocobb']")
@@ -11,6 +12,7 @@ PARSER.add_argument('--datasplit', nargs=1, type=str, help="split the data into 
 PARSER.add_argument('--detect', nargs=3, type=str, help="detect on videos, using trained model. arg1['aabb', 'obb', 'ocaabb', 'ocobb'],  arg2[RELATIVE PATH TO WEIGHTS], arg3['detect', 'track]")
 PARSER.add_argument('--augment',nargs=1, type=str, help="add augmented images to dataset arg1['aug', 'test']")
 PARSER.add_argument('--rotate', nargs=3, type=str, help="rotate img. arg1[RELATIVE PATH TO VID], arg2[VIDEO FILE NAME], arg3['90', '180', '270']")
+PARSER.add_argument('--frag', nargs=3, type=str, help="cut x sec fragment for REM dataset. arg1[video file name], arg2[patient id], arg3[start time]")
 
 if __name__ == '__main__':
     args = PARSER.parse_args() # type: ignore
@@ -32,6 +34,8 @@ if __name__ == '__main__':
             dataaugmenter.test_transformed_bboxes()
     elif(args.rotate):
         imgtransformer.rotate_img(args.rotate[0], args.rotate[1], args.rotate[2])
+    elif(args.frag):
+        videocutter.cut_video(args.frag[0], args.frag[1], args.frag[2])
 
        
 
---
+++ videocutter.py
@@ -0,0 +1,25 @@
+from moviepy.video.io.VideoFileClip import VideoFileClip
+import os
+
+def cut_video(file_name, patient_id, start_time):
+    input_file =  os.path.join(os.path.abspath(os.getcwd()), "vid", "frag", "IN", file_name)
+    output_file = os.path.join(os.path.abspath(os.getcwd()), "vid", "frag", patient_id, file_name+"T"+start_time)
+
+    try:
+        video = VideoFileClip(input_file)
+
+        end_time = start_time + 1  # 1 second after the start time
+
+        if start_time < 0 or end_time > video.duration:
+            raise ValueError("Start time is out of the video duration range.")
+
+        cut_clip = video.subclip(start_time, end_time)
+
+        cut_clip.write_videofile(output_file, codec="libx264", audio_codec="aac")
+
+        print(f"Video segment saved to {output_file}")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        video.close()
