c8a45c14cff77fa91b0c598da4a6475d1025962e
Lisaag <lisatgroen@hotmail.com>
2025-01-09 11:37:02 +0100

    debug

--- videocutter.py
+++ videocutter.py
@@ -7,21 +7,22 @@ def cut_video(file_name:str, patient_id:str, start_time:str):
     output_file = os.path.join(os.path.abspath(os.getcwd()), "vid", "frag", patient_id, file_name)
     if not os.path.exists(output_file):
         os.makedirs(output_file)
-    # try:
-    #     video = VideoFileClip(input_file)
+    try:
+        video = VideoFileClip(input_file)
+        start_time=float(start_time)
 
-    #     end_time = start_time + 1  # 1 second after the start time
+        end_time = start_time + 1  # 1 second after the start time
 
-    #     if start_time < 0 or end_time > video.duration:
-    #         raise ValueError("Start time is out of the video duration range.")
+        if start_time < 0 or end_time > video.duration:
+            raise ValueError("Start time is out of the video duration range.")
 
-    #     cut_clip = video.subclip(start_time, end_time)
+        cut_clip = video.subclip(start_time, end_time)
 
-    #     cut_clip.write_videofile(output_file, codec="libx264", audio_codec="aac")
+        cut_clip.write_videofile(output_file, codec="libx264", audio_codec="aac")
 
-    #     print(f"Video segment saved to {output_file}")
+        print(f"Video segment saved to {output_file}")
 
-    # except Exception as e:
-    #     print(f"An error occurred: {e}")
-    # finally:
-    #     video.close()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        video.close()
