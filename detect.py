9cc065ef1c39803c2b0c049ee78445477ba4bd56
Lisaag <lisatgroen@hotmail.com>
2025-01-22 22:03:56 +0100

    first new vers for REM dataset construction

--- detect.py
+++ detect.py
@@ -126,157 +126,87 @@ def track_vid_aabb(relative_weights_path:str, annotation_type:str="aabb"):
     print(all_boxes)
     return all_boxes
 
-def detect_vid_aabb_filter(box:defaultdict):
+def detect_vid_aabb_filter(box:defaultdict, root_dir:str, file_name:str):
     ratio = 16/9
 
-    IN_directory = os.path.join(os.path.abspath(os.getcwd()), "vid", "IN")
-    for filename in os.listdir(IN_directory):
-        video_output_path =  os.path.join(os.path.abspath(os.getcwd()), "vid", "OUT", "OUT"+str(filename))
-        video_input_path =  os.path.join(os.path.abspath(os.getcwd()), "vid", "IN", filename)
-        frame_output_path =  os.path.join(os.path.abspath(os.getcwd()), "vid", "frag", filename[0:len(filename)-4])
+    video_output_path =  os.path.join(root_dir, "cropped", file_name)
+    video_input_path =  os.path.join(root_dir, "raw", file_name)
+    frame_output_path =  os.path.join(root_dir, "frames", file_name[0:len(file_name)-4])
 
-        if not os.path.exists(frame_output_path):
-            os.makedirs(frame_output_path)
-        # Open the video file
-        cap = cv2.VideoCapture(video_input_path)
+    if not os.path.exists(frame_output_path):
+        os.makedirs(frame_output_path)
+    # Open the video file
+    cap = cv2.VideoCapture(video_input_path)
 
-        # Get video properties
-        frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        fps = int(cap.get(cv2.CAP_PROP_FPS))
+    # Get video properties
+    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
 
-        # Define the codec and create VideoWriter object
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        out = cv2.VideoWriter(video_output_path, fourcc, fps, (frame_width, frame_height))
+    # Define the codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(video_output_path, fourcc, fps, (frame_width, frame_height))
 
-        current_frame = 0
-        num_boxes = 0
-        frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    current_frame = 0
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-        frame_indices = np.linspace(0, frame_count-1, 6, dtype=int)
+    frame_indices = np.linspace(0, frame_count-1, 6, dtype=int)
 
 
-        if filename in box:
-            keys = list(box[filename].keys())
-            center_index = len(keys) // 2  # Integer division
-            center_key = keys[center_index]
-            x1, y1, x2, y2 = box[filename][center_key]
-            height = abs(y1 - y2)
-            width = height * ratio
-            x_center = (x1 + x2) / 2
-            x1 = int(x_center - width / 2)
-            x2 = int(x_center + width / 2)
-        else:
-            print(f'Tracking info of file {filename} not found!!')
+    if file_name in box:
+        keys = list(box[file_name].keys())
+        center_index = len(keys) // 2  # Integer division
+        center_key = keys[center_index]
+        x1, y1, x2, y2 = box[file_name][center_key]
+        height = abs(y1 - y2)
+        width = height * ratio
+        x_center = (x1 + x2) / 2
+        x1 = int(x_center - width / 2)
+        x2 = int(x_center + width / 2)
+    else:
+        print(f'Tracking info of file {file_name} not found!!')
 
-        
-        out2 = cv2.VideoWriter(os.path.join(os.path.abspath(os.getcwd()), "vid", "OUT", "OUTCROP"+str(filename)), fourcc, fps, (x2-x1+1, y2-y1+1))
+    
+    out2 = cv2.VideoWriter(os.path.join(os.path.abspath(os.getcwd()), "vid", "OUT", "OUTCROP"+str(file_name)), fourcc, fps, (x2-x1+1, y2-y1+1))
 
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-        # Process each frame
-        while cap.isOpened():
-            ret, frame = cap.read()
-            if not ret:
-                break
+        out2.write(frame[y1:y2+1, x1:x2+1])
 
-            out2.write(frame[y1:y2+1, x1:x2+1])
-
-            if np.isin(current_frame, frame_indices):
-                print(os.path.join(frame_output_path, "FRAME" + str(current_frame) + ".jpg"))
-                cv2.imwrite(os.path.join(frame_output_path, "FRAME" + str(current_frame) + ".jpg"), frame[y1:y2+1, x1:x2+1])
+        #save sequence of frames
+        if np.isin(current_frame, frame_indices):
+            print(os.path.join(frame_output_path, "FRAME" + str(current_frame) + ".jpg"))
+            cv2.imwrite(os.path.join(frame_output_path, "FRAME" + str(current_frame) + ".jpg"), frame[y1:y2+1, x1:x2+1])
 
 
 
-            if box[filename].get(current_frame) != None:
-                # top-left corner and bottom-right corner of rectangle
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        if box[file_name].get(current_frame) != None:
+            # top-left corner and bottom-right corner of rectangle
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-            out.write(frame)
+        out.write(frame)
 
-            current_frame += 1
-        
-
-        print(f'Frame count: {frame_count}')
-        # Release resources
-        cap.release()
-        out.release()
-        cv2.destroyAllWindows()
-        print(f"Processed video saved at {video_output_path}")
-
-
-def detect_vid_aabb(relative_weights_path:str):
-    weights_path = os.path.join(os.path.abspath(os.getcwd()), relative_weights_path)
-    model = YOLO(weights_path)
-
-    IN_directory = os.path.join(os.path.abspath(os.getcwd()), "vid", "IN")
-    OUT_directory = os.path.join(os.path.abspath(os.getcwd()), "vid", "OUT")
-    for filename in os.listdir(IN_directory):
-        print(filename)
-        video_output_path =  os.path.join(os.path.abspath(os.getcwd()), "vid", "OUT", "OUT"+str(filename))
-        video_input_path =  os.path.join(os.path.abspath(os.getcwd()), "vid", "IN", filename)
-        # Open the video file
-        cap = cv2.VideoCapture(video_input_path)
-
-        # Get video properties
-        frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        fps = int(cap.get(cv2.CAP_PROP_FPS))
-
-        # Define the codec and create VideoWriter object
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        out = cv2.VideoWriter(video_output_path, fourcc, fps, (frame_width, frame_height))
-
-
-        frame_count = 0
-        # Process each frame
-        while cap.isOpened():
-            ret, frame = cap.read()
-            if not ret:
-                break
-            frame_count+=1
-
-            # Make predictions
-            results = model(frame, verbose=False)
-
-
-            # Draw predictions on the frame
-            for result in results:  # Iterate through detections
-                boxes = result.boxes  # Get bounding boxes
-                for box in boxes:
-                    x1, y1, x2, y2 = map(int, box.xyxy[0])  # Bounding box coordinates
-                    conf = box.conf[0]  # Confidence score
-                    cls = box.cls[0]  # Class index
-
-                    boxColor = (255, 0, 0)
-                    if cls == 0: boxColor = (0, 255, 0)
-                    # Draw bounding box
-                    cv2.rectangle(frame, (x1, y1), (x2, y2), boxColor, 2)
-                    label = f"{model.names[int(cls)]} {conf:.2f}"  # Class label and confidence
-                    cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.0, boxColor, 2)
-
-            # Write the frame to the output video
-            out.write(frame)
-
-            # Optional: Display the frame (comment out if not needed)
-            # cv2.imshow('YOLO Prediction', frame)
-            # if cv2.waitKey(1) & 0xFF == ord('q'):
-            #     break
-
-        # Release resources
-        cap.release()
-        out.release()
-        cv2.destroyAllWindows()
-        print(f"Processed video saved at {video_output_path}")
+        current_frame += 1
+    
+    # Release resources
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+    print(f"Processed video saved at {video_output_path}")
 
 
 
-def detect_vid(annotation_type:str, relative_weights_path:str, detection_type:str):
+def detect_vid(annotation_type:str, relative_weights_path:str, patient_nr:str):
     if(annotation_type == "aabb" or annotation_type == "ocaabb"):
-        if (detection_type == "detect"):
-            detect_vid_aabb(relative_weights_path)
-        elif (detection_type == "track"):
-            all_boxes = track_vid_aabb(relative_weights_path, annotation_type)
-            detect_vid_aabb_filter(all_boxes)
+        root_dir:str = os.path.join(os.path.abspath(os.getcwd()), "frags", patient_nr)
+        for eye_state_folder in os.listdir(root_dir):
+            fragment_dir:str = os.path.join(root_dir, eye_state_folder, "raw")
+            for fragment_file in os.listdir(fragment_dir):
+                all_boxes = track_vid_aabb(relative_weights_path, annotation_type)
+                detect_vid_aabb_filter(all_boxes, os.path.join(root_dir, eye_state_folder), fragment_file)
     else:
         print("annotation type [" + annotation_type + "] not recognized")
 
