fcd1f875f7f5482418eb53b99f0dfd30b2c15843
Lisaag <lisatgroen@hotmail.com>
2025-01-24 23:04:33 +0100

    bug fix

--- REMsplitter.py
+++ REMsplitter.py
@@ -12,6 +12,6 @@ def split_REM_set():
                     continue
                 for fragment_dir in os.listdir(data_dir):
                     for eye_data_dir in os.listdir(os.path.join(data_dir, fragment_dir)):
-                        if(os.path.isdir(os.listdir(os.path.join(data_dir, fragment_dir, eye_data_dir)))):
+                        if(os.path.isdir(os.path.join(data_dir, fragment_dir, eye_data_dir))):
                            frames_dir = os.path.join(data_dir, fragment_dir, eye_data_dir, eye_data_dir, "frames", fragment_dir)
                            print(frames_dir)
