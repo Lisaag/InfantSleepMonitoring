a285f527b82c150736aa7001f029f72ca62a681c
Lisaag <lisatgroen@hotmail.com>
2025-01-27 17:10:01 +0100

    debug

--- REMmodel.py
+++ REMmodel.py
@@ -9,17 +9,17 @@ def create_3dcnn_model(input_shape=(6, 64, 64, 1), num_classes=2):
         layers.Conv3D(32, kernel_size=(3, 3, 3), activation='relu', padding='same', input_shape=input_shape),
         layers.MaxPooling3D(pool_size=(2, 2, 2)),
 
-        # Second 3D Convolutional Layer
-        layers.Conv3D(64, kernel_size=(3, 3, 3), activation='relu', padding='same'),
-        layers.MaxPooling3D(pool_size=(2, 2, 2)),
+        # # Second 3D Convolutional Layer
+        # layers.Conv3D(64, kernel_size=(3, 3, 3), activation='relu', padding='same'),
+        # layers.MaxPooling3D(pool_size=(2, 2, 2)),
 
-        # Third 3D Convolutional Layer
-        layers.Conv3D(128, kernel_size=(3, 3, 3), activation='relu', padding='same'),
-        layers.MaxPooling3D(pool_size=(2, 2, 2)),
+        # # Third 3D Convolutional Layer
+        # layers.Conv3D(128, kernel_size=(3, 3, 3), activation='relu', padding='same'),
+        # layers.MaxPooling3D(pool_size=(2, 2, 2)),
 
         # Flatten and Fully Connected Layers
         layers.Flatten(),
-        layers.Dense(128, activation='relu'),
+        layers.Dense(32, activation='relu'),
         layers.Dropout(0.5),
         layers.Dense(num_classes, activation='softmax')
     ])
