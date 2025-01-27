770cacb521cfd6d9cd493489de7a97b6621b500f
Lisaag <lisatgroen@hotmail.com>
2025-01-27 17:53:06 +0100

    bugfix

--- REMmodel.py
+++ REMmodel.py
@@ -19,7 +19,7 @@ def create_3dcnn_model(input_shape=(1, 6, 64, 64), num_classes=2):
 
         # Flatten and Fully Connected Layers
         layers.Flatten(),
-        layers.Dense(32, activation='relu'),
+        layers.Dense(128, activation='relu'),
         layers.Dropout(0.5),
         layers.Dense(num_classes, activation='softmax')
     ])
