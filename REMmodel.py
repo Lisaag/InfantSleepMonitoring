d2be3b0a467b0532969ad243131e873dcbc46781
Lisaag <lisatgroen@hotmail.com>
2025-01-27 17:52:22 +0100

    re-added layers

--- REMmodel.py
+++ REMmodel.py
@@ -9,13 +9,13 @@ def create_3dcnn_model(input_shape=(1, 6, 64, 64), num_classes=2):
         layers.Conv3D(32, kernel_size=(3, 3, 3), activation='relu', padding='same', input_shape=input_shape),
         layers.MaxPooling3D(pool_size=(2, 2, 2)),
 
-        # # Second 3D Convolutional Layer
-        # layers.Conv3D(64, kernel_size=(3, 3, 3), activation='relu', padding='same'),
-        # layers.MaxPooling3D(pool_size=(2, 2, 2)),
+        # Second 3D Convolutional Layer
+        layers.Conv3D(64, kernel_size=(3, 3, 3), activation='relu', padding='same'),
+        layers.MaxPooling3D(pool_size=(2, 2, 2)),
 
-        # # Third 3D Convolutional Layer
-        # layers.Conv3D(128, kernel_size=(3, 3, 3), activation='relu', padding='same'),
-        # layers.MaxPooling3D(pool_size=(2, 2, 2)),
+        # Third 3D Convolutional Layer
+        layers.Conv3D(128, kernel_size=(3, 3, 3), activation='relu', padding='same'),
+        layers.MaxPooling3D(pool_size=(2, 2, 2)),
 
         # Flatten and Fully Connected Layers
         layers.Flatten(),
