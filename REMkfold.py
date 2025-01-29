f3da8a4b9d9f91301bf288ac3a2dfa734c7f07d8
Lisaag <lisatgroen@hotmail.com>
2025-01-29 23:29:24 +0100

    REM hp

--- REMkfold.py
+++ REMkfold.py
@@ -26,7 +26,7 @@ def create_3dcnn_model(input_shape=(1, 6, 64, 64), num_classes=2):
 
         # Flatten and Fully Connected Layers
         layers.Flatten(),
-        layers.Dense(64, activation='relu', kernel_regularizer=regularizers.L2(0.02), kernel_initializer=tf.keras.initializers.HeNormal()),
+        layers.Dense(64, activation='relu', kernel_regularizer=regularizers.L2(0.005), kernel_initializer=tf.keras.initializers.HeNormal()),
         # layers.BatchNormalization(momentum=0.99),
         layers.Dropout(0.5),
         layers.Dense(num_classes, activation='softmax')
