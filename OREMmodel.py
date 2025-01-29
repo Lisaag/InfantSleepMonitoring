670bcb8308cb946d46594ed11de9bd455b51d867
Lisaag <lisatgroen@hotmail.com>
2025-01-29 19:44:28 +0100

    changed batch size

--- OREMmodel.py
+++ OREMmodel.py
@@ -116,7 +116,7 @@ def REMtrain():
 
    
 
-    history = model.fit(train_samples_stacked, train_labels_bce, validation_data=(val_samples_stacked, val_labels_bce), epochs=50, batch_size=2)
+    history = model.fit(train_samples_stacked, train_labels_bce, validation_data=(val_samples_stacked, val_labels_bce), epochs=50, batch_size=32)
 
 
     # 2. Get predictions
