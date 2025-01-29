c2c91596f35781c4b739e86da947edd869702c8c
Lisaag <lisatgroen@hotmail.com>
2025-01-29 01:24:52 +0100

    debug

--- gpu_test.py
+++ gpu_test.py
@@ -1,4 +1,9 @@
 import tensorflow as tf
+
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+os.environ["SM_FRAMEWORK"] = "tf.keras"
+
 import keras_tuner
 
 print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
