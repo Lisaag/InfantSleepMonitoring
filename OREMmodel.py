b609507f935a9a368c585d03a1e7fc5f0fe7b5d9
Lisaag <lisatgroen@hotmail.com>
2025-01-29 20:19:25 +0100

    added batchnorm

--- OREMmodel.py
+++ OREMmodel.py
@@ -14,11 +14,13 @@ def create_3dcnn_model(input_shape=(1, 6, 64, 64), num_classes=2):
         # First 3D Convolutional Layer
         layers.Conv3D(32, kernel_size=(1, 3, 3), activation='relu', padding='same', input_shape=input_shape),
         layers.Conv3D(32, kernel_size=(3, 3, 3), activation='relu', padding='same'),
+        layers.BatchNormalization(),
         layers.MaxPooling3D(pool_size=(2, 2, 2)),
 
 
         # Second 3D Convolutional Layer
         layers.Conv3D(64, kernel_size=(3, 3, 3), activation='relu', padding='same'),
+        layers.BatchNormalization(),
         layers.MaxPooling3D(pool_size=(2, 2, 2)),   
         layers.Dropout(0.5),
 
@@ -29,6 +31,7 @@ def create_3dcnn_model(input_shape=(1, 6, 64, 64), num_classes=2):
         # Flatten and Fully Connected Layers
         layers.Flatten(),
         layers.Dense(64, activation='relu', kernel_regularizer=regularizers.L2(0.01), kernel_initializer=tf.keras.initializers.HeNormal()),
+        layers.BatchNormalization(),
         layers.Dropout(0.8),
         layers.Dense(num_classes, activation='softmax')
     ])
