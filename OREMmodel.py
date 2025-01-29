21e1dca46d0f345bd1eec7b31245a7c8500bc546
Lisaag <lisatgroen@hotmail.com>
2025-01-29 17:02:19 +0100

    Orem no dropout

--- OREMmodel.py
+++ OREMmodel.py
@@ -15,12 +15,10 @@ def create_3dcnn_model(input_shape=(1, 6, 64, 64), num_classes=2):
         layers.Conv3D(32, kernel_size=(1, 3, 3), activation='relu', padding='same', input_shape=input_shape),
         layers.Conv3D(32, kernel_size=(3, 3, 3), activation='relu', padding='same'),
         layers.MaxPooling3D(pool_size=(2, 2, 2)),
-        layers.Dropout(0.25),
 
         # Second 3D Convolutional Layer
         layers.Conv3D(64, kernel_size=(3, 3, 3), activation='relu', padding='same'),
         layers.MaxPooling3D(pool_size=(2, 2, 2)),   
-        layers.Dropout(0.25),
 
         # # Third 3D Convolutional Layer
         # layers.Conv3D(128, kernel_size=(3, 3, 3), activation='relu', padding='same'),
@@ -29,7 +27,6 @@ def create_3dcnn_model(input_shape=(1, 6, 64, 64), num_classes=2):
         # Flatten and Fully Connected Layers
         layers.Flatten(),
         layers.Dense(64, activation='relu'),
-        layers.Dropout(0.5),
         layers.Dense(num_classes, activation='softmax')
     ])
 
