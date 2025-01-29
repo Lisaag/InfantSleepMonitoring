0a803e813f23f76d186ae31b9e3cc94a53758465
Lisaag <lisatgroen@hotmail.com>
2025-01-29 20:22:31 +0100

    rem reg

--- OREMmodel.py
+++ OREMmodel.py
@@ -28,7 +28,7 @@ def create_3dcnn_model(input_shape=(1, 6, 64, 64), num_classes=2):
 
         # Flatten and Fully Connected Layers
         layers.Flatten(),
-        layers.Dense(64, activation='relu', kernel_regularizer=regularizers.L2(0.01), kernel_initializer=tf.keras.initializers.HeNormal()),
+        layers.Dense(64, activation='relu', kernel_initializer=tf.keras.initializers.HeNormal()),
         layers.BatchNormalization(),
         layers.Dropout(0.5),
         layers.Dense(num_classes, activation='softmax')
