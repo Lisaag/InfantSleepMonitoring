eccfd057af0e008d927af4e702debdfb74e06d0e
Lisaag <lisatgroen@hotmail.com>
2025-02-14 11:50:07 +0100

    25 percent reduced database

--- datasplitter.py
+++ datasplitter.py
@@ -202,7 +202,7 @@ def create_splits(split_type):
     print(f'VAL O:{len(val_split.open_samples)} - C:{len(val_split.closed_samples)} OCCLUDED O:{len(val_split.open_samples_occ)} - C:{len(val_split.closed_samples_occ)}')
     print(f'TEST O:{len(test_split.open_samples)} - C:{len(test_split.closed_samples)} OCCLUDED O:{len(test_split.open_samples_occ)} - C:{len(test_split.closed_samples_occ)}')
 
-    train_samples, val_samples, test_samples = reduce_splits(train_split, val_split, test_split, 100)
+    train_samples, val_samples, test_samples = reduce_splits(train_split, val_split, test_split, 25)
 
     for sample in train_samples:
        label_file = re.sub(r'\.jpg$', '', sample) + ".txt"
