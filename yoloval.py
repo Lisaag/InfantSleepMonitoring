13d38e64a562539d78487f4cdfeddb1a317b3248
Lisaag <lisatgroen@hotmail.com>
2025-02-19 22:15:11 +0100

    bugfix

--- yoloval.py
+++ yoloval.py
@@ -7,6 +7,6 @@ model = YOLO("runs/detect/train5/weights/best.pt")
 validation_results = model.val(data="SLAPIaabb.yaml", imgsz=640, split="test", save_json=True, device="0")
 
 print(f'map50: {validation_results.box.map50}')
-print(f'precision: {validation_results.box.precision}')
-print(f'recall: {validation_results.box.recall}')
+print(f'precision: {validation_results.box.p}')
+print(f'recall: {validation_results.box.r}')
 print(f'f1: {validation_results.box.f1}')
