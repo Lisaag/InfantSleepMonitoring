6d2801082f43f1092cbf15c80eb50f04c2d53941
Lisaag <lisatgroen@hotmail.com>
2025-02-19 22:10:05 +0100

    val

--- yoloval.py
+++ yoloval.py
@@ -6,4 +6,7 @@ model = YOLO("runs/detect/train5/weights/best.pt")
 # Customize validation settings
 validation_results = model.val(data="SLAPIaabb.yaml", imgsz=640, split="test", save_json=True, device="0")
 
-print(validation_results.box.map50)
+print(f'map50: {validation_results.box.map50}')
+print(f'precision: {validation_results.box.precision}')
+print(f'recall: {validation_results.box.recall}')
+print(f'f1: {validation_results.box.f1}')
