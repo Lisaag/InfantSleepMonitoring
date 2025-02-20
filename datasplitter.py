d2c906478b430d14dd811446b99ed486c7dcd2ad
Lisaag <lisatgroen@hotmail.com>
2025-02-20 01:10:29 +0100

    bugfix

--- datasplitter.py
+++ datasplitter.py
@@ -193,16 +193,16 @@ def create_splits(split_type):
             else: curr_split.closed_samples.append(df_all["filename"][i])
             total_samples_filtered += 1
             filtered.append(df_all["filename"][i])
-
-            x, y, w, h = get_aabb_from_string(df_all["region_shape_attributes"][i])
-            x=x+(w/2); y=y+(h/2)
-            image = cv2.imread(os.path.join(os.path.abspath(os.getcwd()), all_images_dir, df_all["filename"][i]))
-            height, width, _ = image.shape  
-            x/=width; w/=width; y/=height; h/=height
-            write_aabb_label(df_all["filename"][i], all_labels_dir, x, y, w, h, "0")
         else:
             if(attributes[0]): curr_split.open_samples_occ.append(df_all["filename"][i])
             else: curr_split.closed_samples_occ.append(df_all["filename"][i])
+
+        x, y, w, h = get_aabb_from_string(df_all["region_shape_attributes"][i])
+        x=x+(w/2); y=y+(h/2)
+        image = cv2.imread(os.path.join(os.path.abspath(os.getcwd()), all_images_dir, df_all["filename"][i]))
+        height, width, _ = image.shape  
+        x/=width; w/=width; y/=height; h/=height
+        write_aabb_label(df_all["filename"][i], all_labels_dir, x, y, w, h, "0")
 
     print(f'TOTAL FILES {len(set(all))}')
     print(f'TOTAL FILTERED FILES {len(set(filtered))}')
