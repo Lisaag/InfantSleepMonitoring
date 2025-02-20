0197833129e4a57bc5a4a377f03b3f68bbb00fb8
Lisaag <lisatgroen@hotmail.com>
2025-02-20 16:54:51 +0100

    train

--- yolotrain.py
+++ yolotrain.py
@@ -16,7 +16,17 @@ model.train(
     data="SLAPIaabb.yaml",
     epochs=100,
     imgsz=640,
-    augment=False,
     patience=15,
-    device=0
+    device=0,
+    fliplr=0.0,
+    mosaic=0.0,
+    erasing=0.0,
+    scale=0.2,
+    hsv_h=0.0,
+    hsv_s=0.2,
+    hsv_v=0.4,
+    translate=0.1,
+    crop_fraction=0.8
+    
+
 )
