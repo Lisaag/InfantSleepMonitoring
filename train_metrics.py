8994c61f3329f8d2f7657bcbad41620a7c7f1cd9
Lisaag <lisatgroen@hotmail.com>
2025-02-26 22:39:10 +0100

    plot style and dpi

--- train_metrics.py
+++ train_metrics.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 def plot_loss_curve(train_losses, val_losses):
     epochs = range(1, len(train_losses) + 1)
 
-    plt.style.use("seaborn-darkgrid") 
+    plt.style.use("seaborn-v0_8-darkgrid") 
     
     plt.figure(figsize=(8, 5))
     plt.plot(epochs, train_losses, label='Training Loss', marker='o')
@@ -17,7 +17,7 @@ def plot_loss_curve(train_losses, val_losses):
     plt.title('Training and Validation Loss')
     plt.legend()
     plt.grid(True)
-    plt.savefig(os.path.join(os.path.abspath(os.getcwd()),"tmp.jpg"), format='jpg')   
+    plt.savefig(os.path.join(os.path.abspath(os.getcwd()),"tmp.jpg"), dpi=300, format='jpg')   
 
 def get_loss(train_metrics, train_box, i):
     if(i == 0): return 0
