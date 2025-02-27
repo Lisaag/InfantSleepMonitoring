4535f123f9ff6289de535f1a99971c2281d3f9ee
Lisaag <lisatgroen@hotmail.com>
2025-02-27 13:06:52 +0100

    debug

--- yolotsne.py
+++ yolotsne.py
@@ -7,8 +7,8 @@ model = YOLO(os.path.join(os.path.abspath(os.getcwd()), "runs", "AUG", "default-
 
 
 
-#image_path = os.path.join(os.path.abspath(os.getcwd()), "datasets", "SLAPI", "aug", "test", "images", "frame_CG_360_29-03-2022-7.jpg")
-image_path = os.path.join(os.path.abspath(os.getcwd()), "datasets", "SLAPI", "aug", "test", "images", "frame_417_12-10-2022-47.jpg")
+image_path = os.path.join(os.path.abspath(os.getcwd()), "datasets", "SLAPI", "aug", "test", "images", "frame_CG_360_29-03-2022-7.jpg")
+#image_path = os.path.join(os.path.abspath(os.getcwd()), "datasets", "SLAPI", "aug", "test", "images", "frame_417_12-10-2022-47.jpg")
 # Get the penultimate fully connected layer
 def get_penultimate_fc_output(module, input, output):
     # Save the output of the second-to-last fully connected layer
