13eb763285ae01b9bf38ea9a512cb18767749cb7
Lisaag <lisatgroen@hotmail.com>
2025-02-27 14:03:47 +0100

    debug

--- yolotsne.py
+++ yolotsne.py
@@ -14,14 +14,14 @@ def get_penultimate_fc_output(module, input, output):
     # Save the output of the second-to-last fully connected layer
     #print(f'SHAPE {input.size()}')
     #print(input)  # This will print the output of the layer
-    print(f'SHAPE {len(output)}')
-    print(output)
-    print(module)
+    print(f'SHAPE {output.size}')
+    #print(output)
+    #print(module)
     
 
 # Register the hook
 print(f'MODEL {len(model.model.model)}')
-model.model.model[-1].register_forward_hook(get_penultimate_fc_output)
+model.model.model[-2].register_forward_hook(get_penultimate_fc_output)
 
 # Now, run a sample image through the model to get the output
 results = model(image_path)  # Run the image through the model
