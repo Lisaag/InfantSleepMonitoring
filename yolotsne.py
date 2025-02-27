a70e2472642f1e3084a964bb0a6f06bcde72abc7
Lisaag <lisatgroen@hotmail.com>
2025-02-27 13:14:56 +0100

    debug

--- yolotsne.py
+++ yolotsne.py
@@ -12,12 +12,12 @@ image_path = os.path.join(os.path.abspath(os.getcwd()), "datasets", "SLAPI", "au
 # Get the penultimate fully connected layer
 def get_penultimate_fc_output(module, input, output):
     # Save the output of the second-to-last fully connected layer
-    print(f'SHAPE {output.size()}')
-    print(output)  # This will print the output of the layer
+    print(f'SHAPE {input.size()}')
+    print(input)  # This will print the output of the layer
     
 
 # Register the hook
-model.model.model[-2].register_forward_hook(get_penultimate_fc_output)
+model.model.model[-1].register_forward_hook(get_penultimate_fc_output)
 
 # Now, run a sample image through the model to get the output
 results = model(image_path)  # Run the image through the model
