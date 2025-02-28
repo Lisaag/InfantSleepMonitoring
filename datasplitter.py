ee8720551b23384b788edb0bb4a2f4cce55801bc
Lisaag <lisatgroen@hotmail.com>
2025-02-28 16:58:39 +0100

    plot

--- datasplitter.py
+++ datasplitter.py
@@ -77,7 +77,7 @@ def get_aabb_from_string(input_string: str):
 
 def plot_dataset_info(simple_train, simple_val, dif_train, dif_val, dif_test):
     
-    ticks = [1, 2, 3, 4, 5]
+    ticks = [1, 2, 3.5, 4.5, 5.5]
     bar_names = ["train", "val", "train", "val", "test"]
     open = [lst[0] for lst in [simple_train, simple_val, dif_train, dif_val, dif_test]]
     closed = [lst[1] for lst in [simple_train, simple_val, dif_train, dif_val, dif_test]]
@@ -89,7 +89,7 @@ def plot_dataset_info(simple_train, simple_val, dif_train, dif_val, dif_test):
     plt.legend(loc="upper right")
 
     group_divider = 2.5 
-    plt.axvline(x=group_divider, color='black', linestyle='-', linewidth=1.5)
+    plt.axvline(x=group_divider, color='black', linestyle='-', linewidth=1.0)
 
     plt.savefig(os.path.join(os.path.abspath(os.getcwd()),"dataset.jpg"), dpi=300, format='jpg')   
    
