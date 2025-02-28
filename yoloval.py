d396623fe806d3e9b2a79181ef4c01cc1d62cc70
Lisaag <lisatgroen@hotmail.com>
2025-03-01 00:54:59 +0100

    val

--- yoloval.py
+++ yoloval.py
@@ -8,6 +8,11 @@ validation_results = model.val(data="occ.yaml", imgsz=640, split="test", save_js
 
 print(f'map50: {validation_results.box.ap50}')
 print(f'map95: {validation_results.box.ap}')
-print(f'precision: {validation_results.box.p}')
-print(f'recall: {validation_results.box.r}')
-print(f'f1: {validation_results.box.f1}')
+print(f'box precision: {validation_results.box.p}')
+print(f'box recall: {validation_results.box.r}')
+print(f'box f1: {validation_results.box.f1}')
+
+
+print(f'cls precision: {validation_results.cls.p}')
+print(f'cls recall: {validation_results.cls.r}')
+print(f'cls f1: {validation_results.cls.f1}')
