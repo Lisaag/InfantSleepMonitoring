6f080a4b6d5d93429be7195fb729e117ea29cdb3
Lisaag <lisatgroen@hotmail.com>
2025-02-28 17:34:04 +0100

    bugfix

--- datasplitter.py
+++ datasplitter.py
@@ -95,8 +95,8 @@ def plot_dataset_info(simple_train, simple_val, dif_train, dif_val, dif_test):
     group_divider = 3
     plt.axvline(x=group_divider, color='grey', linestyle='--', linewidth=1.0)
     
-    plt.text(1.5, -6, "Simple dataset", ha='center', va='center', fontsize=8)
-    plt.text(5.0, -6, "Difficult dataset", ha='center', va='center', fontsize=8)
+    plt.text(1.5, -30, "Simple dataset", ha='center', va='center', fontsize=10)
+    plt.text(5.0, -30, "Difficult dataset", ha='center', va='center', fontsize=10)
 
     plt.savefig(os.path.join(os.path.abspath(os.getcwd()),"dataset.jpg"), dpi=300, format='jpg')   
    
