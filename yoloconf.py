2ddc126f65053829638109312fbfe5ee46cbd6ea
Lisaag <lisatgroen@hotmail.com>
2025-03-06 23:43:16 +0100

    warn

--- yoloconf.py
+++ yoloconf.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import os
 import seaborn as sns
 import numpy as np
+import warnings
+
 
 def plot_confusion_matrix():
     data = [[437, 13], [30, np.nan]]
@@ -9,7 +11,9 @@ def plot_confusion_matrix():
     sns.set_theme(font_scale=1.0)  # for label size
     # 4. Plot confusion matrix using seaborn for better visualization
     plt.figure(figsize=(12, 9), tight_layout=True)
-    h = sns.heatmap(data, annot=True, fmt='d', cmap='Blues', vmin=1, xticklabels=np.arange(2), yticklabels=np.arange(2), annot_kws={"size": 8})
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")  # suppress empty matrix RuntimeWarning: All-NaN slice encountered
+        h = sns.heatmap(data, annot=True, fmt='d', cmap='Blues', xticklabels=np.arange(2), yticklabels=np.arange(2), annot_kws={"size": 8}, square=True, vmin=0.0)
     h.set_xticklabels(['eye', 'background'])
     h.set_yticklabels(['eye', 'background'])
     plt.xlabel('True')
