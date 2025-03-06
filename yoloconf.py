3a268f0406ca8c24fa10260c0a9f961a65d5c651
Lisaag <lisatgroen@hotmail.com>
2025-03-06 23:10:22 +0100

    mask

--- yoloconf.py
+++ yoloconf.py
@@ -5,15 +5,17 @@ import numpy as np
 
 def plot_confusion_matrix():
     data =[[437, 13], [30, 0]]
+    mask = data == 0
 
     # 4. Plot confusion matrix using seaborn for better visualization
     plt.figure(figsize=(10, 7))
-    h = sns.heatmap(data, annot=True, fmt='d', cmap='Blues', xticklabels=np.arange(2), yticklabels=np.arange(2))
+    h = sns.heatmap(data, annot=True, fmt='d', cmap='Blues', mask=mask, xticklabels=np.arange(2), yticklabels=np.arange(2))
     h.set_xticklabels(['eye', 'background'])
     h.set_yticklabels(['eye', 'background'])
     plt.xlabel('True')
     plt.ylabel('Predicted')
     plt.title('Confusion Matrix')
+
 
     plt.savefig(os.path.join(os.path.abspath(os.getcwd()),"train_plots", "confusion_matrix.jpg"), format='jpg')  
 
