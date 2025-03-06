5bcdf133494ba33b71b90bd81541384f3723f3f5
Lisaag <lisatgroen@hotmail.com>
2025-03-06 20:50:54 +0100

    val

--- yoloval.py
+++ yoloval.py
@@ -1,10 +1,10 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("runs/AUG/no-aug3/weights/best.pt")
+model = YOLO("runs/OC/open-closed/weights/best.pt")
 
 # Customize validation settings
-validation_results = model.val(data="occ.yaml", imgsz=640, split="test", save_json=True, device="0", iou=0.5)
+validation_results = model.val(data="OC.yaml", imgsz=640, split="test", save_json=True, device="0", iou=0.5)
 
 print(f'map50: {validation_results.box.ap50}')
 print(f'map95: {validation_results.box.ap}')
