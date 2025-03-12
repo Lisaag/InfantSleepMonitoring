a01708590e6eb6598a5d5722b53c200281a432c6
Lisaag <lisatgroen@hotmail.com>
2025-03-12 15:30:46 +0100

    debug

--- REMtrack.py
+++ REMtrack.py
@@ -149,6 +149,8 @@ def save_boxes_csv(boxes:defaultdict, root_dir:str, file_name:str):
 
         box_index += 1
 
+        print(f'Write csv for eye {key}')
+
 
 
 def detect_vid(relative_weights_path:str):
@@ -160,6 +162,7 @@ def detect_vid(relative_weights_path:str):
             fragment_dir:str = os.path.join(patient_dir, eye_state_dir)
             for fragment_file in os.listdir(fragment_dir):
                 all_boxes = track_vid_aabb(relative_weights_path, fragment_dir, fragment_file)
+                print(all_boxes)
                 #detect_vid_aabb_filter(all_boxes, os.path.join(patient_dir, eye_state_dir), fragment_file)
                 save_boxes_csv(all_boxes, os.path.join(frames_dir, patient, eye_state_dir), fragment_file)
                 write_bbox(all_boxes, fragment_dir, os.path.join(frames_dir, patient, eye_state_dir), fragment_file)
