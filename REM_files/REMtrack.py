befa8206f407382cb51a9feb20abd302b7c11961
Lisaag <lisatgroen@hotmail.com>
2025-03-13 11:11:41 +0100

    refactor

--- REM_files/REMtrack.py
+++ REM_files/REMtrack.py
@@ -157,7 +157,6 @@ def detect_vid(relative_weights_path:str):
             fragment_dir:str = os.path.join(patient_dir, eye_state_dir)
             for fragment_file in os.listdir(fragment_dir):
                 all_boxes = track_vid_aabb(relative_weights_path, fragment_dir, fragment_file)
-                #print(dict(all_boxes))
                 save_boxes_csv(all_boxes, os.path.join(frames_dir, patient, eye_state_dir), fragment_file)
                 write_bbox(all_boxes, fragment_dir, os.path.join(frames_dir, patient, eye_state_dir), fragment_file)
 
