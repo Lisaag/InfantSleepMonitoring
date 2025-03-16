4991b9164c88c023cbbeb9d2349dff6c5a359c4d
Lisaag <lisatgroen@hotmail.com>
2025-03-16 19:02:35 +0100

    bla

--- REM_files/OREMmodel.py
+++ REM_files/OREMmodel.py
@@ -50,8 +50,8 @@ def REMtrain():
     #val_ids = ['554', '778'] #fold1
     #val_ids = ['004', '866'] #fold2
     #val_ids = ['399', '657'] #fold3
-    val_ids = ['416', '875'] #fold4
-    #val_ids = ['614', '704'] #fold5
+    #val_ids = ['416', '875'] #fold4
+    val_ids = ['614', '704'] #fold5
     val_samples = list(); val_labels = list(); train_samples = list(); train_labels = list()
 
     for patient in os.listdir(data_dir):
