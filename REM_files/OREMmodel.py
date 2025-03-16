ee53f35f9867789defb4752d0dfa7ab43528c38b
Lisaag <lisatgroen@hotmail.com>
2025-03-16 18:35:33 +0100

    different fold

--- REM_files/OREMmodel.py
+++ REM_files/OREMmodel.py
@@ -48,7 +48,10 @@ def REMtrain():
 
     is_OREM = True
     #val_ids = ['554', '778'] #fold1
-    val_ids = ['004', '866']
+    #val_ids = ['004', '866'] #fold2
+    val_ids = ['399', '657'] #fold3
+    #val_ids = ['416', '875'] #fold4
+    #val_ids = ['614', '704'] #fold5
     val_samples = list(); val_labels = list(); train_samples = list(); train_labels = list()
 
     for patient in os.listdir(data_dir):
