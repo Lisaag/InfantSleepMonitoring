9849fc2ec40710c991bbe370dd6ea2cb98a5aae8
Lisaag <lisatgroen@hotmail.com>
2025-03-17 14:22:57 +0100

    hp change

--- REM_files/OREMmodel.py
+++ REM_files/OREMmodel.py
@@ -28,7 +28,7 @@ def create_3dcnn_model(input_shape=(1, 6, 64, 64), num_classes=2):
         layers.Dense(num_classes, activation='softmax')
     ])
 
-    optimizer = keras.optimizers.Adam(lr=0.00008)
+    optimizer = keras.optimizers.Adam(lr=0.00006)
     # Compile the model
     model.compile(optimizer=optimizer,
                   loss=keras.losses.BinaryCrossentropy(from_logits=False),
