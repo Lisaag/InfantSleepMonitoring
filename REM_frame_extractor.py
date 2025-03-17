29824915896e0cce0d5bc45c4fa50a99a00cedd4
Lisaag <lisatgroen@hotmail.com>
2025-03-17 22:05:01 +0100

    oepsie

--- REM_frame_extractor.py
+++ REM_frame_extractor.py
@@ -148,11 +148,6 @@ def extract_frames(video_dir:str, file_name:str, csv_dir:str, patient_id:str, RE
 
 
     frame_indices = np.linspace(min_bounds, max_bounds, frame_stack_count, dtype=int).tolist()
-    print(f'frame count {frame_count} ')
-    print(f'frame indices {file_name.replace(".mp4", "")+suffix}   - {frame_indices} ')
-
-    #print(f'Frame indices {frame_indices}')
-
 
     center_frames_dir = os.path.join(cropped_dir, "center", patient_id, REMclass, file_name.replace(".mp4", "")+suffix)
     if not os.path.exists(center_frames_dir): os.makedirs(center_frames_dir)
@@ -167,44 +162,24 @@ def extract_frames(video_dir:str, file_name:str, csv_dir:str, patient_id:str, RE
     center_vid = cv2.VideoWriter(os.path.join(center_frames_dir, "center.mp4"), fourcc, fps, (frame_width, frame_height))
     interpolate_vid = cv2.VideoWriter(os.path.join(interpolate_frames_dir, "interpolate.mp4"), fourcc, fps, (frame_width, frame_height))
     every_vid = cv2.VideoWriter(os.path.join(every_frames_dir, "every.mp4"), fourcc, fps, (frame_width, frame_height))
-    #every_vid = cv2.VideoWriter(every_frames_dir, fourcc, fps, (frame_width, frame_height))
-    
-    #TODO return array of n evenly spaced integers between 3 and framecount-3 (bc of augmentation offset)
-    #TODO return array of center pos 
-    #TODO return array of interpolate between first and last
-    #TODO return array of every YOLO frame
+
 
     current_frame = 0
-
-    #print(f'LEN {len(center_frames)}')
-    #print(f'FRAMES {frame_count}')
 
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
-        current_frame+=1
-        if(current_frame < min_bounds or current_frame > max_bounds): continue
+        if(current_frame < min_bounds or current_frame > max_bounds):
+            current_frame+=1
+            continue
 
         save_frame_stack(frame.copy(), center_vid, current_frame, frame_indices, center_frames[current_frame - min_bounds], center_frames_dir)
         save_frame_stack(frame.copy(), interpolate_vid, current_frame, frame_indices, interpolate_frames[current_frame - min_bounds], interpolate_frames_dir)
         save_frame_stack(frame.copy(), every_vid, current_frame, frame_indices, every_frames[current_frame - min_bounds], every_frames_dir)
 
-        # x1, y1, x2, y2 = center_frames[current_frame - min_bounds]
+        current_frame+=1
 
-        # #save stack of frames
-        # if np.isin(current_frame, frame_indices):
-        #     cv2.imwrite(os.path.join(center_frames_dir, "FRAME" + str(current_frame) + ".jpg"), frame[y1:y2, x1:x2])  
-        #     cv2.imwrite(os.path.join(interpolate_frames_dir, "FRAME" + str(current_frame) + ".jpg"), frame[y1:y2, x1:x2])  
-
-
-        # #show crop in original video for debugging
-        # #frame_center = frame.copy()
-        # #frame_interpolate = frame.copy()
-        # #frame_every = frame.copy()
-
-        # cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-        # center_vid.write(frame)
 
     cap.release()
     center_vid.release()
