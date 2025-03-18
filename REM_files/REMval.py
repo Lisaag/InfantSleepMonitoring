06524a4e7d16bb75da0c025835dad3876ec12747
Lisaag <lisatgroen@hotmail.com>
2025-03-18 14:38:16 +0100

    val debug

--- REM_files/REMval.py
+++ REM_files/REMval.py
@@ -78,7 +78,7 @@ with open(os.path.join(os.path.abspath(os.getcwd()),"REM-results", "true_labels.
 
 model2 = tf.keras.Model(inputs=model.input, outputs=model.layers[-2].output)
 features = model2(val_samples_stacked)
-#tsne = TSNE(n_components=2).fit_transform(features)
+tsne = TSNE(n_components=2).fit_transform(features)
 
 
 
