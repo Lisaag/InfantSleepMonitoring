7ad437c60341e4fb9dbcb0b236f3c596dc23a0bb
Lisaag <lisatgroen@hotmail.com>
2025-03-19 15:39:19 +0100

    changed split

--- REM_files/settings.py
+++ REM_files/settings.py
@@ -8,7 +8,8 @@ is_OREM = False
 #val_ids = ['416', '440'] #fold4
 #val_ids = ['614', '704'] #fold5
 
-val_ids = ['554', '778'] #REM, OREM goed
+val_ids = ['399', '704', '866'] #REM , OREM 
+#val_ids = ['554', '778'] #REM goed, OREM goed
 #val_ids = ['614', '657'] #REM acceptabel, OREM heel goed
 
 
