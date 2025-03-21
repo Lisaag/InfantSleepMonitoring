5672b806a2ec5dcebc464f210d6f85d4d406fa3b
Lisaag <lisatgroen@hotmail.com>
2025-03-21 17:30:52 +0100

    bugfix?

--- REM_files/REMtrain.py
+++ REM_files/REMtrain.py
@@ -56,7 +56,7 @@ def create_3dcnn_model(lr = 0.0001, dropout=0.5, l2=0.5, input_shape=(1, 6, 64, 
         layers.Dense(64, activation='relu', kernel_regularizer=regularizers.L2(l2), kernel_initializer=tf.keras.initializers.HeNormal()),
         layers.BatchNormalization(),
         layers.Dropout(dropout),
-        layers.Dense(num_classes, activation='sigmoid')
+        layers.Dense(1, activation='sigmoid')
     ])
 
     optimizer = keras.optimizers.Adam(lr=lr)
