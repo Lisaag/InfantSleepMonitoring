69cf23fb5f9cb75f510256ab780322f52f9dabda
Lisaag <lisatgroen@hotmail.com>
2025-03-24 23:27:09 +0100

    batch size

--- REM_files/settings.py
+++ REM_files/settings.py
@@ -32,7 +32,7 @@ checkpoint_filename = "checkpoint.model.keras"
 # train_l2 = [0.01, 0.1, 1.0]
 # train_dropout = [0.3, 0.5, 0.7]
 
-train_batch_size = [16]
+train_batch_size = [2]
 train_initial_lr = [0.0001]
 train_l2 = [0.08]
 train_dropout = [0.4]
