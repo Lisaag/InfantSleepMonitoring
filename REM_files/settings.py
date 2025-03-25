28d22925b6db131387b71489cb90efc312f304df
Lisaag <lisatgroen@hotmail.com>
2025-03-25 16:03:02 +0100

    bs64lr0.0003

--- REM_files/settings.py
+++ REM_files/settings.py
@@ -32,8 +32,8 @@ checkpoint_filename = "checkpoint.model.keras"
 # train_l2 = [0.01, 0.1, 1.0]
 # train_dropout = [0.3, 0.5, 0.7]
 
-train_batch_size = [32]
-train_initial_lr = [0.0004]
+train_batch_size = [64]
+train_initial_lr = [0.0003]
 train_l2 = [0.08]
 train_dropout = [0.4]
 
