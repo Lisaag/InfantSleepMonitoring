90c7e50efc90ea4445372bdad0fc6a9407a40786
Lisaag <lisatgroen@hotmail.com>
2025-03-26 16:41:49 +0100

    group norm

--- REM_files/REMtrain.py
+++ REM_files/REMtrain.py
@@ -57,12 +57,12 @@ def create_model(lr = 0.0001, dropout=0.3, l2=0.1, input_shape=(1, 6, 64, 64), s
         layers.Conv3D(32, kernel_size=(1, 3, 3), activation='relu', padding='same', input_shape=input_shape),
         layers.Conv3D(32, kernel_size=(3, 3, 3), activation='relu', padding='same'),
         layers.MaxPooling3D(pool_size=(2, 2, 2)),
-        layers.BatchNormalization(),
+        layers.GroupNormalization(8),
         layers.Dropout(dropout, seed=seed),
 
         layers.Conv3D(64, kernel_size=(3, 3, 3), activation='relu', padding='same'),
         layers.MaxPooling3D(pool_size=(2, 2, 2)),
-        layers.BatchNormalization(),
+        layers.GroupNormalization(8),
         layers.Dropout(dropout, seed=seed),
 
         layers.Flatten(),
