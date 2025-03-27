079cf0eaa2065c0342f9a5db06e363f9ae08cd6a
Lisaag <lisatgroen@hotmail.com>
2025-03-27 19:19:50 +0100

    reset dat aaugmentation settings

--- REM_files/REMtrain.py
+++ REM_files/REMtrain.py
@@ -57,14 +57,17 @@ def create_model(lr = 0.0001, dropout=0.3, l2=0.1, input_shape=(1, 6, 64, 64), s
         layers.Conv3D(32, kernel_size=(1, 3, 3), activation='relu', padding='same', input_shape=input_shape),
         layers.Conv3D(32, kernel_size=(3, 3, 3), activation='relu', padding='same'),
         layers.MaxPooling3D(pool_size=(2, 2, 2)),
+        layers.BatchNormalization(),
 
         layers.Conv3D(64, kernel_size=(3, 3, 3), activation='relu', padding='same'),
         layers.MaxPooling3D(pool_size=(2, 2, 2)),
-        layers.Dropout(dropout, seed=seed),
+        layers.BatchNormalization(),
+        #layers.Dropout(dropout, seed=seed),
 
         layers.Flatten(),
         layers.Dense(64, activation='relu', kernel_regularizer=regularizers.L2(l2), kernel_initializer=tf.keras.initializers.HeNormal(seed=seed)),
-        layers.Dropout(dropout * 1.5, seed=seed),
+        layers.BatchNormalization(),
+        #layers.Dropout(dropout * 1.5, seed=seed),
         layers.Dense(1, activation='sigmoid')
     ])
 
