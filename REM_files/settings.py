0e2ae2d586025da6ce68344aa0b05bbc468acd5d
Lisaag <lisatgroen@hotmail.com>
2025-03-28 22:43:49 +0100

    higher batch size

--- REM_files/settings.py
+++ REM_files/settings.py
@@ -33,8 +33,8 @@ checkpoint_filename = "checkpoint.model.keras"
 # train_l2 = [0.01, 0.1, 1.0]
 # train_dropout = [0.3, 0.5, 0.7]
 
-train_batch_size = [8]
-train_initial_lr = [0.0002]
+train_batch_size = [16]
+train_initial_lr = [0.00025]
 train_l2 = [0.05]
 train_dropout = [0.5]
 
