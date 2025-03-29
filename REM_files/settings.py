595fcc254959fc94fe8907d7152b91194a140d14
Lisaag <lisatgroen@hotmail.com>
2025-03-29 14:57:15 +0100

    increase initial learning rate

--- REM_files/settings.py
+++ REM_files/settings.py
@@ -35,7 +35,7 @@ checkpoint_filename = "checkpoint.model.keras"
 # train_dropout = [0.3, 0.5, 0.7]
 
 train_batch_size = [64]
-train_initial_lr = [0.0003]
+train_initial_lr = [0.00035]
 train_l2 = [0.04]
 train_dropout = [0.6]
 
