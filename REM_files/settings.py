a1358d20162e6c1c750202853651824586cfcb44
Lisaag <lisatgroen@hotmail.com>
2025-03-30 23:23:50 +0200

    CREM aug interpolate

--- REM_files/settings.py
+++ REM_files/settings.py
@@ -9,18 +9,18 @@ is_OREM = False
 #val_ids = ['614', '657'] #REM acceptabel, OREM heel goed
 
 
-val_ids = [['416', '778'],
-           ['004', '704', '773'],
-           ['399', '875', '440'],
-           ['554', '866'],
-           ['614', '657']] 
-#val_ids = [['004', '704', '773']]
+# val_ids = [['416', '778'],
+#            ['004', '704', '773'],
+#            ['399', '875', '440'],
+#            ['554', '866'],
+#            ['614', '657']] 
+val_ids = [['614', '657']]
 
 #val_ids = ['416', '875'] #<-- SLECHTE FOLD EXAMPLE
 
 frame_stack_count = 6
 
-data_dir = os.path.join(os.path.abspath(os.getcwd()),"REM", "raw", "cropped", "every")
+data_dir = os.path.join(os.path.abspath(os.getcwd()),"REM", "raw", "cropped", "interpolate")
 results_dir = os.path.join(os.path.abspath(os.getcwd()),"REM-results")
 
 model_filename = "model_architecture.json"
@@ -36,7 +36,7 @@ checkpoint_filename = "checkpoint.model.keras"
 
 train_batch_size = [32]
 train_initial_lr = [0.00025]
-train_l2 = [0.2]
+train_l2 = [0.08, 0.1, 0.15, 0.2]
 train_dropout = [0.6]
 
 seeds = [0]
