2a778f52445c947fe5882b6d92d09a201984214b
Lisaag <lisatgroen@hotmail.com>
2025-03-31 18:34:29 +0200

    more l2

--- REM_files/settings.py
+++ REM_files/settings.py
@@ -37,7 +37,7 @@ checkpoint_filename = "checkpoint.model.keras"
 
 train_batch_size = [8]
 train_initial_lr = [0.00015]
-train_l2 = [0.04]
-train_dropout = [0.6]
+train_l2 = [0.2]
+train_dropout = [0.1]
 
 seeds = [6, 7, 8, 9, 10, 11, 12, 13]
