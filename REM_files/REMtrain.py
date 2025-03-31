2991f7369e93429cde36f2cc02fe297a9ba8a32b
Lisaag <lisatgroen@hotmail.com>
2025-03-31 17:30:39 +0200

    less filters

--- REM_files/REMtrain.py
+++ REM_files/REMtrain.py
@@ -55,17 +55,17 @@ def save_model_json(model, path):
 
 def create_model(lr = 0.0001, dropout=0.3, l2=0.1, input_shape=(1, 6, 64, 64), seed = 0):
     model = models.Sequential([
-        layers.Conv3D(32, kernel_size=(1, 3, 3), padding='same',activation='elu', input_shape=input_shape),
-        layers.Conv3D(32, kernel_size=(3, 3, 3), activation='elu', padding='valid'),
+        layers.Conv3D(8, kernel_size=(1, 3, 3), padding='same',activation='elu', input_shape=input_shape),
+        layers.Conv3D(8, kernel_size=(3, 3, 3), activation='elu', padding='valid'),
         layers.Dropout(dropout, seed=seed),
         layers.MaxPooling3D(pool_size=(2, 2, 2)),
        
-        layers.Conv3D(64, kernel_size=(3, 3, 3), activation='elu', padding='same'),
+        layers.Conv3D(16, kernel_size=(3, 3, 3), activation='elu', padding='same'),
         layers.Dropout(dropout, seed=seed),
         layers.MaxPooling3D(pool_size=(2, 4, 4)),
 
         layers.Flatten(),
-        layers.Dense(64, activation='relu', kernel_initializer=tf.keras.initializers.he_normal(seed=seed)),
+        layers.Dense(16, activation='relu', kernel_initializer=tf.keras.initializers.he_normal(seed=seed)),
         layers.Dense(1, activation='sigmoid')
     ])
 
