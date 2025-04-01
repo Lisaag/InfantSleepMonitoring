fc276c319143a010e61cb41335785b5913022eb2
Lisaag <lisatgroen@hotmail.com>
2025-04-02 00:56:38 +0200

    OREM test

--- REM_files/settings.py
+++ REM_files/settings.py
@@ -2,12 +2,12 @@ import os
 
 is_OREM = False
 
-val_ids = [['416', '778'],
-           ['004', '704', '773'],
-           ['399', '875', '440'],
-           ['554', '866'],
-           ['614', '657']] 
-#val_ids = [['399', '875', '440']]
+# val_ids = [['416', '778'],
+#            ['004', '704', '773'],
+#            ['399', '875', '440'],
+#            ['554', '866'],
+#            ['614', '657']] 
+val_ids = [['416', '778']]
 
 frame_stack_count = 6
 img_size = 64
@@ -29,8 +29,8 @@ checkpoint_filename = "checkpoint.model.keras"
 train_batch_size = [16]
 train_initial_lr = [0.00018]
 train_l2 = [0.04]
-train_dropout = [0.5]
+train_dropout = [0.2]
 
-seeds = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+#seeds = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 #seeds = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-#seeds = [0]
+seeds = [0]
