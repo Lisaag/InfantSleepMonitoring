0161f328062df0c36d1990cf476dd9a873d5497d
Lisaag <lisatgroen@hotmail.com>
2025-04-01 15:23:00 +0200

    bf

--- REM_files/boxplot.py
+++ REM_files/boxplot.py
@@ -19,9 +19,9 @@ def make_boxplot(data, classes):
 
     sns.boxplot(x="Fold", y="AP", hue="Fold", data=df, palette=palette, width=0.6, legend=False, fill=False)
     sns.stripplot(x="Folds", y="AP", hue="Folds", palette=palette,  data=df, color="black", jitter=True, alpha=0.6, legend=False)
-    for i, category in enumerate(data['Category'].unique()):
-        plt.scatter([i - 0.2] * data[data['Fold'] == category].shape[0], 
-                    data[data['Fold'] == category]['AP'], 
+    for i, fold in enumerate(df['Fold'].unique()):
+        plt.scatter([i - 0.2] * df[df['Fold'] == fold].shape[0], 
+                    df[df['Fold'] == fold]['AP'], 
                     color='black', alpha=0.6, s=50)
 
     plt.title("AP per fold over 10 train runs")
