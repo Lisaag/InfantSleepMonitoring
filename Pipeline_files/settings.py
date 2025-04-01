c624753e7b1827ad41db8bb90ac43253867f8b7b
Lisaag <lisatgroen@hotmail.com>
2025-04-01 18:24:10 +0200

    ey loc path

--- Pipeline_files/settings.py
+++ Pipeline_files/settings.py
@@ -4,5 +4,6 @@ fragment_length = 90
 
 cur_vid = "223-processed.mov"
 
+eye_loc_path = os.path.join(os.path.abspath(os.getcwd()), "PIPELINE", "loc")
 yolo_weights_path = os.path.join(os.path.abspath(os.getcwd()), "runs", "OC", "open-closed", "weights", "best.pt")
 video_path = os.path.join(os.path.abspath(os.getcwd()), cur_vid)
