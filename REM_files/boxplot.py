9f68c1e508ca503923811cb960438b8e5bbe4592
Lisaag <lisatgroen@hotmail.com>
2025-04-01 14:44:57 +0200

    boxplot

--- REM_files/boxplot.py
+++ REM_files/boxplot.py
@@ -10,13 +10,16 @@ import numpy as np
 def make_boxplot(data, classes):
     plt.figure()
 
+    palette = sns.color_palette("husl", 5)
+
     df = pd.DataFrame({
     "Fold": classes,
     "AP": data
     })
-    sns.boxplot(x="Fold", y="AP", data=df)
+    sns.boxplot(x="Fold", y="AP", data=df, palette=palette)
+    sns.stripplot(x="Fold", y="AP", data=df, color="black", jitter=True, alpha=0.6)
 
-    plt.title("Grouped Box Plots with Seaborn")
+    plt.title("AP per fold over 10 train runs")
     plt.savefig(os.path.join(settings.results_dir, "boxplot.jpg"), format='jpg', dpi=500)  
 
 
