5b5ff43c5033419a5c7415f62c9dbbc63df29dd4
Lisaag <lisatgroen@hotmail.com>
2025-04-01 18:23:54 +0200

    writing pos to csv

--- Pipeline_files/settings.py
+++ Pipeline_files/settings.py
@@ -2,5 +2,7 @@ import os
 
 fragment_length = 90
 
+cur_vid = "223-processed.mov"
+
 yolo_weights_path = os.path.join(os.path.abspath(os.getcwd()), "runs", "OC", "open-closed", "weights", "best.pt")
-video_path = os.path.join(os.path.abspath(os.getcwd()), "223-processed.mov")
+video_path = os.path.join(os.path.abspath(os.getcwd()), cur_vid)
