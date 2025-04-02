7629aef9d133ddf0886a09f1f9f23b0330d28180
Lisaag <lisatgroen@hotmail.com>
2025-04-03 00:39:02 +0200

    hp change

--- REM_files/settings.py
+++ REM_files/settings.py
@@ -29,9 +29,9 @@ checkpoint_filename = "checkpoint.model.keras"
 # train_dropout = [0.3, 0.5, 0.7]
 
 train_batch_size = [16]
-train_initial_lr = [0.00015]
+train_initial_lr = [0.00018]
 train_l2 = [0.04]
-train_dropout = [0.5]
+train_dropout = [0.7]
 
 seeds = [0]
 #seeds = [1, 2, 3, 4, 5, 6, 7, 8, 9]
