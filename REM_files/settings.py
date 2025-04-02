a08480eef629b405d90d7675a02a5a6e7c35afb8
Lisaag <lisatgroen@hotmail.com>
2025-04-03 00:42:55 +0200

    Center combined

--- REM_files/settings.py
+++ REM_files/settings.py
@@ -3,18 +3,18 @@ import os
 is_OREM = True
 is_combined = True
 
-# val_ids = [['416', '778'],
-#            ['004', '704', '773'],
-#            ['399', '875', '440'],
-#            ['554', '866'],
-#            ['614', '657']] 
 val_ids = [['416', '778'],
-           ['004', '704', '773']]
+           ['004', '704', '773'],
+           ['399', '875', '440'],
+           ['554', '866'],
+           ['614', '657']] 
+# val_ids = [['416', '778'],
+#            ['004', '704', '773']]
 
 frame_stack_count = 6
 img_size = 64
 
-data_dir = os.path.join(os.path.abspath(os.getcwd()),"REM", "raw", "cropped", "every")
+data_dir = os.path.join(os.path.abspath(os.getcwd()),"REM", "raw", "cropped", "center")
 results_dir = os.path.join(os.path.abspath(os.getcwd()),"REM-results")
 
 model_filename = "model_architecture.json"
@@ -31,7 +31,7 @@ checkpoint_filename = "checkpoint.model.keras"
 train_batch_size = [16]
 train_initial_lr = [0.00018]
 train_l2 = [0.04]
-train_dropout = [0.7]
+train_dropout = [0.5]
 
 seeds = [0]
 #seeds = [1, 2, 3, 4, 5, 6, 7, 8, 9]
