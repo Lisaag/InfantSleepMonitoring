8ff03fd3cc342c81179eb2df0a0accee6fd4ccd5
Lisaag <lisatgroen@hotmail.com>
2025-04-02 10:59:13 +0200

    process 929

--- Pipeline_files/settings.py
+++ Pipeline_files/settings.py
@@ -2,7 +2,7 @@ import os
 
 fragment_length = 45
 
-cur_vid = "417-processed.mp4"
+cur_vid = "929-processed.mp4"
 
 eye_loc_path = os.path.join(os.path.abspath(os.getcwd()), "PIPELINE", "loc")
 yolo_weights_path = os.path.join(os.path.abspath(os.getcwd()), "runs", "OC", "open-closed", "weights", "best.pt")
