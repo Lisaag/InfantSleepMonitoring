4750094d7342a57d205516b1496b0b9d5b2aefe0
Lisaag <lisatgroen@hotmail.com>
2025-04-06 15:41:32 +0200

    thresholds

--- Pipeline_files/sleep_predictor.py
+++ Pipeline_files/sleep_predictor.py
@@ -22,9 +22,9 @@ from sklearn.metrics import confusion_matrix
 
 max_movement_fraction = 0.5
 
-REM_threshold = 0.725 #threshold of when fragment is classified as REM
+REM_threshold = 0.65 #threshold of when fragment is classified as REM
 O_threshold = 5 * (settings.fragment_length//45) #threshold of O count when fragment is classified as O
-AS_REM_count = 5 #number of REMs in a minute to be classified as AS
+AS_REM_count = 10 #number of REMs in a minute to be classified as AS
 W_O_count = 20 #number os O in am inute to be classified as W
 
 frag_per_min = 40
