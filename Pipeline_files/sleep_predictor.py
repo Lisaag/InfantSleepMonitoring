66db3d98fe24333536ccfc9702e0d26a31142f79
Lisaag <lisatgroen@hotmail.com>
2025-04-07 20:07:17 +0200

    sleep predictor

--- Pipeline_files/sleep_predictor.py
+++ Pipeline_files/sleep_predictor.py
@@ -20,7 +20,7 @@ from matplotlib.patches import Patch
 import seaborn as sns
 from sklearn.metrics import confusion_matrix
 
-max_movement_fraction = 1.0
+max_movement_fraction = 0.9
 
 CREM_threshold = 0.3 #threshold of when fragment is classified as REM
 OREM_threshold = 0.3#threshold of when fragment is classified as REM
@@ -28,7 +28,7 @@ OREM_threshold = 0.3#threshold of when fragment is classified as REM
 
 REM_threshold = 0.3 #threshold of when fragment is classified as REM
 O_threshold = 0 * (settings.fragment_length//45) #threshold of O count when fragment is classified as O
-AS_REM_count = 5#number of REMs in a minute to be classified as AS
+AS_REM_count = 4#number of REMs in a minute to be classified as AS
 W_O_count = 5 #number os O in am inute to be classified as W
 
 frag_per_min = 40
