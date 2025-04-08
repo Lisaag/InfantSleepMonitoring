132ec4148531fd60817ea0c31942daf339f6451b
Lisaag <lisatgroen@hotmail.com>
2025-04-08 09:25:33 +0200

    conf

---
+++ confusion_matrix.py
@@ -0,0 +1,29 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+def plot_confusion_matrix(true_labels = list(), predicted_labels = list()):
+    print(true_labels)
+    print(predicted_labels)
+    filtered_true_labels = []
+    filtered_predicted_labels = []
+    for i in (range(len(predicted_labels))):
+        if predicted_labels[i] != 'reject':
+            filtered_predicted_labels.append(predicted_labels[i])
+            filtered_true_labels.append(true_labels[i])
+
+    cm = [[137, 21, 2],[14, 37, 0],[5, 0, 14]]
+    plt.figure(figsize=(10, 7))
+    h = sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=np.arange(3), yticklabels=np.arange(3), annot_kws={"size": 16})
+    ticklabels = ['AS', 'QS', 'W']
+    h.set_xticklabels(ticklabels)
+    h.set_yticklabels(ticklabels)
+    
+    plt.xlabel('Predicted Labels')
+    plt.ylabel('True Labels')
+    plt.title('Confusion Matrix')
+
+    plt.savefig(os.path.join(os.path.abspath(os.getcwd()), "confusion_matrix.jpg"), format='jpg', dpi=500)  
+
+plot_confusion_matrix()
