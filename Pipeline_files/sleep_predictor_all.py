fbf0bc077e808f3befbf65fe0372f49c8f98085c
Lisaag <lisatgroen@hotmail.com>
2025-04-11 10:20:42 +0200

    a

--- Pipeline_files/sleep_predictor_all.py
+++ Pipeline_files/sleep_predictor_all.py
@@ -29,7 +29,7 @@ max_movement_fraction = 0.9
 
 CREM_threshold = 0.55 #threshold of when fragment is classified as REM
 OREM_threshold = 0.75#threshold of when fragment is classified as REM
-
+ 
 REM_threshold = 0.7 #threshold of when fragment is classified as REM
 O_threshold = 3 * (settings.fragment_length//45) #threshold of O count when fragment is classified as O
 AS_REM_count = 0#number of REMs in a minute to be classified as AS
