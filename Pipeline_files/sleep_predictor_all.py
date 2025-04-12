8efdac00561be831aad9a400195207efba7ec6ab
Lisaag <lisatgroen@hotmail.com>
2025-04-13 01:14:26 +0200

    a

--- Pipeline_files/sleep_predictor_all.py
+++ Pipeline_files/sleep_predictor_all.py
@@ -32,7 +32,7 @@ OREM_threshold = 0.7#threshold of when fragment is classified as REM
  
 REM_threshold = 0.7 #threshold of when fragment is classified as REM
 O_threshold = 3 * (settings.fragment_length//45) #threshold of O count when fragment is classified as O
-AS_REM_count = 1#number of REMs in a minute to be classified as AS
+AS_REM_count = 2#number of REMs in a minute to be classified as AS
 W_O_count = 5 #number os O in am inute to be classified as Ws
 
 frag_per_min = 40
@@ -135,9 +135,9 @@ def show_prediction_bar(true_classes, prediction_classes, cur_vid, REM_counts):
 
 
     for i, cls in enumerate(prediction_classes):
-        ax.barh(0.15, 1, left=i, color=colors[cls], height=0.1)
+        ax.barh(0.2, 1, left=i, color=colors[cls], height=0.1)
     for i, cls in enumerate(prediction_classes):
-        if cls > 1: continue
+        if cls > 2: continue
         ax.barh(0.125, 1, left=i, color=cmap(norm(REM_counts[i])), height=0.05)
     for i, cls in enumerate(true_classes):
         ax.barh(0, 1, left=i, color=colors[cls], height=0.1)
