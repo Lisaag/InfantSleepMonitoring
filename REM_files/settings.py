213dfec80cd4ad99c0717c50b1c97f9e13be6ee6
Lisaag <lisatgroen@hotmail.com>
2025-04-14 04:42:54 +0200

    no aug OREM

--- REM_files/settings.py
+++ REM_files/settings.py
@@ -14,7 +14,7 @@ val_ids = [['416', '778'],
 frame_stack_count = 6
 img_size = 64
 
-data_dir = os.path.join(os.path.abspath(os.getcwd()),"REM", "raw", "cropped", "every")
+data_dir = os.path.join(os.path.abspath(os.getcwd()),"REM", "raw", "cropped", "center")
 results_dir = os.path.join(os.path.abspath(os.getcwd()),"REM-results")
 
 model_filename = "model_architecture.json"
